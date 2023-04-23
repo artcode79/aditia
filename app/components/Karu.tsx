c0a25de0444aab96ce43ec947dcd4fb0b5818e1d
artcode79 <uewfau@gmail.com>
2023-04-23 10:07:30 +0700

    nyoba

--- app/components/Karu.tsx
+++ app/components/Karu.tsx
@@ -6,65 +6,19 @@ import Link from "next/link";
 export default function Karu() {
   return (
     <>
-      <div
-        id="carousel-captions"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              className="d-block w-100"
-              alt=""
-              src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            />
-            <div className="carousel-caption-background d-none d-md-block"></div>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>Slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img
-              className="d-block w-100"
-              alt=""
-              src="https://images.pexels.com/photos/1213447/pexels-photo-1213447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            />
-            <div className="carousel-caption-background d-none d-md-block"></div>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>Slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
+      <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold text-center">Custom jumbotron</h1>
+          <p className="col-md-8 fs-4 text-center">
+            Using a series of utilities, you can create this jumbotron, just
+            like the one in previous versions of Bootstrap. Check out the
+            examples below for how you can remix and restyle it to your liking.
+          </p>
+          <button className="btn btn-primary btn-lg" type="button">
+            Example button
+          </button>
         </div>
-        <a
-          className="carousel-control-prev"
-          data-bs-target="#carousel-captions"
-          role="button"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          data-bs-target="#carousel-captions"
-          role="button"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </a>
       </div>
-      <Link href="/k/dashboard" className="btn btn-primary">
-        Go Back
-      </Link>
     </>
   );
 }
--- app/k/mahasiswa/page.tsx
+++ app/k/mahasiswa/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Mahasiswa() {
   return (
     <>
       <div className="container">
+        <Link href="/k/mahasiwa/add" className="btn btn-primary">
+          add
+        </Link>
         <div className="card border-dark shadow">
           <table id="example" class="table table-striped" width="100%">
             <thead>
